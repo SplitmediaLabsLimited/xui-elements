e53760b611dea484287fc332ee19c75a6edc4693
Light Lagon <lila@splitmedialabs.com>
2015-12-08 22:07:37 +0800

    feat: Add opacity and border demo to main xbc demo.

--- demos/xui-theme-xbc/js/script.js
+++ demos/xui-theme-xbc/js/script.js
@@ -5,9 +5,11 @@ t.addEventListener('dom-change', function() {
 
   const tabs = document.querySelector('xui-tabs');
   const fontcolor = tabs.querySelector('#fontcolor');
+  const bordercolor = tabs.querySelector('#bordercolor');
+  const outlineselect = tabs.querySelector('#outlineselect');
+  const opacityrange = tabs.querySelector('#opacityrange');
 
-  window.selectfontcolor = function(e) {
-      console.log(e.type);
+  window.selectfontcolor = function() {
       let temparr = [];
       const val = tabs.querySelector('#fontcolor').color;
       const oldval = tabs.querySelector('#contenttext').getAttribute('inputstyle');
@@ -25,6 +27,25 @@ t.addEventListener('dom-change', function() {
       tabs.querySelector('#contenttext').setAttribute('inputstyle', JSON.stringify(temparr));
     };
 
+  window.selectbordercolor = function() {
+      let temparr = [];
+      const val = tabs.querySelector('#bordercolor').color;
+      const border = tabs.querySelector('#outlineselect').value;
+      const oldval = tabs.querySelector('#contenttext').getAttribute('inputstyle');
+      let myobj = JSON.parse(oldval);
+
+      if (oldval !== '') {
+        for (let i = 0; i < myobj.length; i++) {
+          if (myobj[i].prop !== 'border') {
+            temparr.push({'prop': myobj[i].prop, 'val': myobj[i].val});
+          }
+        }
+      }
+
+      temparr.push({'prop': 'border', 'val': border + ' solid ' + val});
+      tabs.querySelector('#contenttext').setAttribute('inputstyle', JSON.stringify(temparr));
+    };
+
   fontcolor.addEventListener('keypress', function(e) {
     if (e.keyCode === 13 || e.keyCode === '13') {
       window.selectfontcolor(e);
@@ -34,6 +55,46 @@ t.addEventListener('dom-change', function() {
   fontcolor.addEventListener('click', function(e) {
     window.selectfontcolor(e);
   });
+
+  bordercolor.addEventListener('click', function(e) {
+      window.selectbordercolor(e);
+  });
+
+  bordercolor.addEventListener('keypress', function(e) {
+    if (e.keyCode === 13 || e.keyCode === '13') {
+      window.selectbordercolor(e);
+    }
+  });
+
+  outlineselect.addEventListener('mouseout', function(e) {
+    window.borderSelect(e);
+  });
+
+  opacityrange.addEventListener('track', function(e) {
+    window.opacityChange(e);
+  });
+
+  opacityrange.addEventListener('click', function(e) {
+    window.opacityChange(e);
+  });
+
+  window.opacityChange = function() {
+    let temparr = [];
+    const opacity = tabs.querySelector('#opacityrange').value;
+    const oldval = tabs.querySelector('#contenttext').getAttribute('inputstyle');
+    let myobj = JSON.parse(oldval);
+
+    if (oldval !== '') {
+      for (let i = 0; i < myobj.length; i++) {
+        if (myobj[i].prop !== 'opacity') {
+          temparr.push({'prop': myobj[i].prop, 'val': myobj[i].val});
+        }
+      }
+    }
+
+    temparr.push({'prop': 'opacity', 'val': '.' + opacity});
+    tabs.querySelector('#contenttext').setAttribute('inputstyle', JSON.stringify(temparr));
+  };
 
   window.customscriptTick = function() {
     const check = tabs.querySelector('#chk_custom');
@@ -68,6 +129,27 @@ t.addEventListener('dom-change', function() {
     } else if (val === 'roboto') {
     temparr.push({'prop': 'font-family', 'val': 'Roboto'});
     }
+
+    tabs.querySelector('#contenttext').setAttribute('inputstyle', JSON.stringify(temparr));
+
+  };
+
+  window.borderSelect = function() {
+    let temparr = [];
+    const val = tabs.querySelector('#outlineselect').value;
+      const color = tabs.querySelector('#bordercolor').color;
+    const oldval = tabs.querySelector('#contenttext').getAttribute('inputstyle');
+
+    let myobj = JSON.parse(oldval);
+
+    if (oldval !== '') {
+      for (let i = 0; i < myobj.length; i++) {
+        if (myobj[i].prop !== 'border') {
+          temparr.push({'prop': myobj[i].prop, 'val': myobj[i].val});
+        }
+      }
+    }
+    temparr.push({'prop': 'border', 'val': val+ ' solid ' + color});
 
     tabs.querySelector('#contenttext').setAttribute('inputstyle', JSON.stringify(temparr));
 
